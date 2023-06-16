0028422c58b5b77732a652f9be11f67246217d9e
Lei Xu <eddyxu@gmail.com>
2023-06-15 20:21:48 -0700

    build

--- rust/benches/diskann.rs
+++ rust/benches/diskann.rs
@@ -13,21 +13,53 @@
 // limitations under the License.
 
 use std::sync::Arc;
+use std::path::Path;
 
 use arrow_array::{FixedSizeListArray, RecordBatch, RecordBatchReader};
 use arrow_schema::{DataType, Field, FieldRef, Schema};
 use criterion::{criterion_group, criterion_main, Criterion};
 use lance::{
-    arrow::RecordBatchBuffer,
+    arrow::*,
     dataset::{WriteMode, WriteParams},
     utils::testing::generate_random_array,
+    index::DatasetIndexExt,
     Dataset,
 };
+
+#[cfg(target_os = "linux")]
 use pprof::criterion::{Output, PProfProfiler};
 
-fn bench_diskann(c: &mut Criterion) {}
+fn bench_diskann(c: &mut Criterion) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
 
-async fn create_dataset(path: &std::path::Path, mode: WriteMode) {
+    let  dataset = rt.block_on(async {
+        create_dataset("./diskann.lance", WriteMode::Create).await
+    })
+
+
+    c.bench_function(
+        &format!("CreateDiskAnnIndex(d=128)"),
+        |b| {
+            b.to_async(&rt).iter(|| async {
+                dataset.
+                let results = dataset
+                    .scan()
+                    .nearest("vector", q, 10)
+                    .unwrap()
+                    .nprobs(10)
+                    .try_into_stream()
+                    .await
+                    .unwrap()
+                    .try_collect::<Vec<_>>()
+                    .await
+                    .unwrap();
+                assert!(results.len() >= 1);
+            })
+        },
+    );
+}
+
+async fn create_dataset(path: impl AsRef<Path>, mode: WriteMode) {
     let schema = Arc::new(Schema::new(vec![Field::new(
         "vector",
         DataType::FixedSizeList(
@@ -46,8 +78,11 @@ async fn create_dataset(path: &std::path::Path, mode: WriteMode) {
                 RecordBatch::try_new(
                     schema.clone(),
                     vec![Arc::new(
-                        FixedSizeListArray::try_new(generate_random_array(num_rows * dim), dim)
-                            .unwrap(),
+                        FixedSizeListArray::try_new(
+                            generate_random_array(num_rows * dim),
+                            dim as i32,
+                        )
+                        .unwrap(),
                     )],
                 )
                 .unwrap()
@@ -55,7 +90,7 @@ async fn create_dataset(path: &std::path::Path, mode: WriteMode) {
             .collect(),
     );
 
-    let test_uri = path.to_str().unwrap();
+    let test_uri = path.as_ref().to_str().unwrap();
     std::fs::remove_dir_all(test_uri).map_or_else(|_| println!("{} not exists", test_uri), |_| {});
     let mut write_params = WriteParams::default();
     write_params.max_rows_per_file = num_rows as usize;
@@ -73,6 +108,13 @@ criterion_group!(
     name=benches;
     config = Criterion::default().significance_level(0.1).sample_size(10)
         .with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = bench_diskann);
+
+// Non-linux version does not support pprof.
+#[cfg(not(target_os = "linux"))]
+criterion_group!(
+    name=benches;
+    config = Criterion::default().significance_level(0.1).sample_size(10);
     targets = bench_diskann);
 
 criterion_main!(benches);
